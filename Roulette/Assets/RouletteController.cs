c82ed7a3c0545af749614db91925545944b52deb
hyojin0326 <114805839+hyojin0326@users.noreply.github.com>
2024-03-18 15:09:43 +0900

    add: roulette

---
+++ Roulette/Assets/RouletteController.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RouletteController : MonoBehaviour
+{
+    float rotSpeed = 0; //회전속도
+    // Start is called before the first frame update
+    void Start()
+    {
+        //프레임레이트를 60으로 고정한다.
+        Application.targetFrameRate = 60;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //클릭하면 회전속도 설정
+        if (Input.GetMouseButtonDown(0))
+        {
+            this.rotSpeed = 10;
+        }
+
+        //회전속도만큼 룰렛을 회전시킨다.
+        transform.Rotate(0, 0, this.rotSpeed);
+
+        //룰렛 감소
+        this.rotSpeed *= 0.96f;
+    }
+}
